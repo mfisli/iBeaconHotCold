f3c25722ac8b8a56a7ccf775dcea98b69e911754
Maks Fisli <mfisli2@gmail.com>
2017-02-11 21:49:46 -0800

    Initial Commit

---
+++ iBeaconHotCold/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconHotCold
+//
+//  Created by Maks on 2017-02-11.
+//  Copyright © 2017 Maks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
