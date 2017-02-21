906d520f174e9cefafd3fe6eb2ffefd86a5c5313
Maks Fisli <mfisli2@gmail.com>
2017-02-20 23:32:08 -0800

    I got the correct UUID. The logic in the code was fine. I just needed the correct key to the beacon. NoteToSelf: UUID is needed param, but major and minor are optional

--- iBeaconHotCold/ViewController.swift
+++ iBeaconHotCold/ViewController.swift
@@ -13,15 +13,23 @@ import CoreBluetooth
 
 class ViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralManagerDelegate {
 
-    var locationManager: CLLocationManager!
-    var blueToothmanager:CBPeripheralManager!
+    var locationManager:  CLLocationManager!
+    var blueToothmanager: CBPeripheralManager!
+    
+    var scanLabelArray = [String]()
+    var arrayIndex = 0
 
     @IBOutlet weak var hotColdLabel: UILabel!
+    
+    @IBOutlet weak var ScanLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("View Did Load")
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scanLabelArray = ["---o---", "--(o)--", "-((o))-",
+                          "(((o)))", "((-o-))", "(--o--)"]
         
         blueToothmanager = CBPeripheralManager(delegate: self, queue: nil)
         
@@ -46,10 +54,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralM
         }
     }
     
-    //default UUID of Kontakt beacons: "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"
+    // MARK: TODO UUID is wrong
+    // Default UUID of Kontakt beacons: "F7826DA6-4FA2-4E98-8024-BC5B71E0893E"
+    // The one I actually needed: "C6C4C829-4FD9-4762-837C-DA24C665015A"
+    // Nevermind - still wrong
+    // omg major: 65535, minor: 0 are optional
     func startScanning() {
-        let uuid = UUID(uuidString: "F7826DA6-4FA2-4E98-8024-BC5B71E0893E")!
-        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 65535, minor: 0, identifier: "kontakt")
+        let uuid = UUID(uuidString: "C6C4C829-4FD9-4762-837C-DA24C665015A")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, identifier: "kontakt")
         
         beaconRegion.notifyOnEntry = true
         beaconRegion.notifyOnExit = true
@@ -57,10 +69,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralM
         locationManager.startMonitoring(for: beaconRegion)
         locationManager.startRangingBeacons(in: beaconRegion)
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if beacons.count > 0 {
-            print(beacons)
+            //print(beacons)
             updateDistance(beacons[0].proximity)
         } else {
             print("No beacons in region")
@@ -68,8 +80,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralM
         }
     }
     
+    // changes the background and the label
+    // to represent the distance of a beacon
     func updateDistance(_ distance: CLProximity) {
-        //print("Updating Distance")
+        
+        if (arrayIndex == scanLabelArray.count - 1){
+            arrayIndex = 0
+            self.ScanLabel.text = self.scanLabelArray[arrayIndex]
+        } else {
+            arrayIndex = arrayIndex + 1
+            self.ScanLabel.text = self.scanLabelArray[arrayIndex]
+        }
+        
         UIView.animate(withDuration: 0.8) {
             switch distance {
             case .unknown:
@@ -91,18 +113,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralM
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        if let beaconRegion = region as? CLBeaconRegion {
-            print("DID ENTER REGION: uuid: \(beaconRegion.proximityUUID.uuidString)")
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-        if let beaconRegion = region as? CLBeaconRegion {
-            print("DID EXIT REGION: uuid: \(beaconRegion.proximityUUID.uuidString)")
-        }
-    }
-    
+    // used to tell the user the status of their blue tooth
     func alert(message: String, title: String = "Blue Tooth Status")
     {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
@@ -139,6 +150,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate, CBPeripheralM
         }
         
         print(statusMessage)
+    }
+    
+    // the following functions are used for debugging
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        if let beaconRegion = region as? CLBeaconRegion {
+            print("DID ENTER REGION: uuid: \(beaconRegion.proximityUUID.uuidString)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        if let beaconRegion = region as? CLBeaconRegion {
+            print("DID EXIT REGION: uuid: \(beaconRegion.proximityUUID.uuidString)")
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error){
